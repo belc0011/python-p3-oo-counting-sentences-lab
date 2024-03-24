02ab9f84a56b824d663b0981e4f483fe2b35fa25
Monica Garcia <monicairenegarcia@gmail.com>
2024-03-23 18:42:58 -0700

    complete counting sentences lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,51 @@
 #!/usr/bin/env python3
 
+
 class MyString:
-  pass
+
+    def __init__(self, value=""):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self, value):
+        if (isinstance(value, str)):
+            self._value = value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        word_length = len(self._value)
+        if (self._value[word_length - 1] == '.'):
+            return True
+        else:
+            return False
+        
+    def is_question(self):
+        word_length = len(self._value)
+        if (self.value[word_length - 1] == '?'):
+            return True
+        else:
+            return False
+    
+    def is_exclamation(self):
+        word_length = len(self._value)
+        if (self.value[word_length - 1] == "!"):
+            return True
+        else:
+            return False
+        
+    def count_sentences(self):
+        count = 0
+        repl_quest = self._value.replace('?', '.')
+        repl_excl = repl_quest.replace('!', '.')
+        repl_dup = repl_excl.replace('...', '.')
+        repl_trip = repl_dup.replace('..', '.')
+        print(repl_quest, repl_excl, repl_dup, repl_trip)
+        for letter in repl_trip:
+            if (letter == '.'):
+                count += 1
+        return count
